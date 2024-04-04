dc76847a967691ca5a3dfbb5436ba821246e5787
AMIT KUMAR <123359563+amitKumar1510@users.noreply.github.com>
2024-04-04 21:16:07 +0530

    Create index.js

---
+++ fileHandling/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const Create = require('./Create');
+const Read = require('./Read');
+const Update = require('./Update');
+const Delete = require('./Delete');
+
+const filePath = 'demofile.txt';
+
+Create(fs,filePath);
+Update(fs,filePath);
+Read(fs,filePath);
+Delete(fs,filePath);
+
